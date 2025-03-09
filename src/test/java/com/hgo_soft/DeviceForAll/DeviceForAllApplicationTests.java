eb23cd92b617da64824ee4a8290817d0b5472cc0
Hugo Jimenez <hgojm.dev@gmail.com>
2025-03-09 16:25:00 -0400

    Added initial java files

---
+++ src/test/java/com/hgo_soft/DeviceForAll/DeviceForAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgo_soft.DeviceForAll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceForAllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
